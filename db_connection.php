e9db46e13e03af11ee3f6818cf07c32c62e0e1f5
BloxiSkid <85189373+imBloxi@users.noreply.github.com>
2024-03-17 18:19:18 +0200

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
